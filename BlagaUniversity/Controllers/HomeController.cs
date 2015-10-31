5c26bbb616e67ffe5b69155e9a69cce6ac02e4dc
Alexandru Calinoiu <calinoiu.alexandru@agilefreaks.com>
2015-10-31 19:57:28 +0200

    adds the about page

--- BlagaUniversity/Controllers/HomeController.cs
+++ BlagaUniversity/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using BlagaUniversity.DAL;
+using BlagaUniversity.ViewModels;
 
 namespace BlagaUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UniversityContext _universityContext = new UniversityContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +16,16 @@ namespace BlagaUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            var enrollmentDateGroups = from student in _universityContext.Students
+                group student by student.EnrollmentDate
+                into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
 
-            return View();
+            return View(enrollmentDateGroups.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +33,16 @@ namespace BlagaUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _universityContext.Dispose();
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
